cc2e51edc110d926c091e33f78991b248d8ef38b
Tolemy Kashyap <kashyaptolemy@gmail.com>
2024-03-26 19:57:44 +0530

    frontend backend integration done

--- src/style-guide/components/Featured/index.jsx
+++ src/style-guide/components/Featured/index.jsx
@@ -1,24 +1,51 @@
-import React from 'react'
-import styles from './style.module.scss'
-import TopRated from '../../../../TopRated.json'
+import React, { useState, useEffect } from 'react';
+import styles from './style.module.scss';
 
 const Featured = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      await fetchData();
+    })()
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:5000/');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      const booksData = data.author.map((author, index) => ({
+        author,
+        book_name: data.book_name[index],
+        image: data.image[index],
+        rating: parseFloat(data.rating[index]).toFixed(2), // Format rating to 2 decimal places
+        votes: data.votes[index]
+      }));
+      setBooks(booksData);
+    } catch (error) {
+      console.error('There was a problem fetching the data:', error);
+    }
+  };
+
   return (
     <div className={styles.container} id="toprated">
       <div className={styles.heading}>TOP 10 BEST RATED BOOKS FOR YOU</div>
       <div className={styles.collection}>
-        {TopRated.slice(0, 10).map((book) => {
-          return (
-            <div className={styles.book_container} key={book["ISBN"]}>
-              <img src={book["Image-URL-M"]} alt={book["Book-Title"]} className={styles.book_cover} />
-              <div className={styles.book_title}>{book["Book-Title"]}</div>
-              <div className={styles.book_author} >by {book["Book-Author"]}</div>
-            </div>
-          );
-        })}
+        {books.slice(0, 10).map((book, index) => (
+          <div className={styles.book_container} key={index}>
+            <img src={book.image} alt={book.book_name} className={styles.book_cover} />
+            <div className={styles.book_title}>{book.book_name}</div>
+            <div className={styles.book_author}>by {book.author}</div>
+            <div className={styles.book_rating}>Rating: {book.rating}</div>
+            <div className={styles.book_votes}>Votes: {book.votes}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
-export default Featured
+export default Featured;
--- src/style-guide/components/Recommend/index.jsx
+++ src/style-guide/components/Recommend/index.jsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react'
 import styles from './style.module.scss'
+import axios from 'axios';
 
 const Recommend = () => {
-  const [bookName, setBookName] = useState();
+  const [bookName, setBookName] = useState('');
+  const [recommendedBooks, setRecommendedBooks] = useState([]);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/api/recommend', { bookName });
+      setRecommendedBooks(response.data.recommendedBooks);
+      console.log(response.data.recommendedBooks
+      )
+    } catch (error) {
+      console.error('Error fetching recommended books:', error);
+    }
+  };
+
+
   return (
     <div className={styles.container} id="recommendation">
       <div className={styles.heading}>Need Recommendation for a book?</div>
       <div className={styles.subcontainer}>
         <div className={styles.subheading}>Enter the name of a book you like</div>
-        <form className={styles.inputform}>
+        <form className={styles.inputform} onSubmit={handleFormSubmit}>
           <input
             type="text"
             placeholder=""
@@ -21,6 +37,15 @@ const Recommend = () => {
             SUBMIT
           </button>
         </form>
+      </div>
+      <div className={styles.recommended_collection}>
+        {recommendedBooks.map((book, index) => (
+          <div className={styles.book_container} key={index}>
+            <img src={book[2]} alt={book.book_name} className={styles.book_cover} />
+            <div className={styles.book_title}>{book[0]}</div>
+            <div className={styles.book_author}>by {book[1]}</div>
+          </div>
+        ))}
       </div>
     </div>
   )
